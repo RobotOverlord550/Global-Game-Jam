861196ca19304f5edea5c0c8f2f3ecb452fc64cb
ERROR-fox <megadracario@gmail.com>
2024-01-26 22:39:34 -0500

    Maps should be somewhat playable

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bounce : MonoBehaviour
+public class Collision : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        CircleCollider2D colide = GetComponent<CircleCollider2D>();
+        BoxCollider2D colide = GetComponent<BoxCollider2D>();
         ContactFilter2D filter = new ContactFilter2D().NoFilter();
         List<Collider2D> results = new List<Collider2D>();
         if (colide.OverlapCollider(filter, results) > 0)
@@ -20,12 +20,5 @@ public class Bounce : MonoBehaviour
     void Update()
     {
         
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        GameObject player = collision.gameObject;
-        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
-        rb.AddForce(-400 * (this.transform.position - player.transform.position));
     }
 }
--- Assets/Scripts/MapSpawner.cs
+++ Assets/Scripts/MapSpawner.cs
@@ -13,17 +13,17 @@ public class MapSpawner : MonoBehaviour
     {
         wall_x = wall.transform.localScale.x;
         wall_y = wall.transform.localScale.y;
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 7; i++)
         {
             GameObject wal = Instantiate(wall);
-            wal.transform.position = new Vector2(Random.Range(-6f, 6f), Random.Range(-5f, 5f));
+            wal.transform.position = new Vector2(Random.Range(-6f, 6f), Random.Range(-4f, 4f));
             wal.transform.Rotate(0.0f, 0.0f, Random.Range(0, 360));
             wal.transform.localScale = new Vector2(wall_x * Random.Range(0.5f, 2f), wall_y * Random.Range(1f, 2f));
         }
         for (int i = 0;i < 5;i++)
         {
             GameObject bounc = Instantiate(bouncer);
-            bounc.transform.position = new Vector2(Random.Range(-6f, 6f), Random.Range(-5f, 5f));
+            bounc.transform.position = new Vector2(Random.Range(-6f, 6f), Random.Range(-4f, 4f));
         }
     }
 
