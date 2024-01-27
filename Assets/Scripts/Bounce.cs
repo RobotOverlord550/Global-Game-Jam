5647dd113d458fb560918e01b26ce04d457960c7
ERROR-fox <megadracario@gmail.com>
2024-01-26 21:57:17 -0500

    Stage setup

---
+++ Assets/Scripts/Bounce.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject player = collision.gameObject;
+        Rigidbody2D rb = player.GetComponent<Rigidbody2D>();
+        rb.AddForce(-400 * (this.transform.position - player.transform.position));
+    }
+}
