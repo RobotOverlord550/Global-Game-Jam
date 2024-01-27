9cf5b4b054a022f017a590b257db9d181e32abc6
ERROR-fox <megadracario@gmail.com>
2024-01-27 14:41:00 -0500

    Menu + scene and audio managers

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager instance;
+    public AudioSource menuMusic;
+    public AudioSource battleMusic;
+    public static AudioManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                Debug.Log("No sound");
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        menuMusic.Play();
+        menuMusic.loop = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
