2346ad1a5d50df78e734e72e0c365e3e6eae5fc4
RobotOverlord550 <erniewhitmarsh3@gmail.com>
2024-01-26 20:37:25 -0800

    bruh

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    private void Update()
+    {
+        handleFollowPlayer();
+    }
+
+    void handleFollowPlayer()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
